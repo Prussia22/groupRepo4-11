22a03e41432726155a41e37a7881012f38aa5c30
Evan Glicker <eaglicker@gmail.com>
2025-04-25 18:57:48 -0400

    turned it into a module

--- main.tf
+++ main.tf
@@ -1,53 +1,44 @@
 terraform {
   required_providers {
     aws = {
-    source = "aws"
+    source = "hashicorp/aws"
     version = "5.94.1"
-    }
   }
 }
-
-locals {
-  #Creates a map of {user = job}
-  user_jobs = {for x in var.users : x.name => x.job}
-  #Finds each unique job type and creates a list
-  job_types = toset([for x in var.users : x.job])
-  #Creates an object where each policy is associated with its job
-  permissions = merge([for prms in var.permissions : {for plc in prms.policies[*] : plc => prms.name}]...)
 }
 
-resource "aws_iam_user" "default" {
-  for_each = local.user_jobs
-  name = each.key
-}
-
-resource "aws_iam_group" "default" {
-  for_each = local.job_types
-  name = each.value
-  depends_on = [ aws_iam_user.default ]
-}
-
-resource "aws_iam_group_membership" "default" {
-  for_each = local.job_types
-  name = "${each.value}-membership"
-  group = each.value
+module "iam_module" {
+  source = "./modules/iam_module"
   users = [
-    for x in var.users : x.name if x.job == each.value
-  ]
-  depends_on = [ aws_iam_group.default ]
-}
-
-resource "aws_iam_account_password_policy" "default" {
-  minimum_password_length        = 9
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
-  allow_users_to_change_password = true
-}
-
-resource "aws_iam_group_policy_attachment" "default" {
-  for_each = local.permissions
-  group = each.value
-  policy_arn = each.key
-  depends_on = [ aws_iam_group.default ]
+    {
+        name = "john_doe"
+        job = "sys_admin"
+    },
+    {
+        name = "alice_smith"
+        job = "db_admin"
+    },
+    {
+        name = "joe_green"
+        job = "ro_user"
+    },
+    {
+        name = "mary_jones"
+        job = "sys_admin"
+    },
+    {
+        name = "thomas_booke"
+        job = "db_admin"
+    }
+]
+permissions = [ {
+  name = "sys_admin"
+  policies = [ "arn:aws:iam::aws:policy/AdministratorAccess" ]
+}, {
+  name = "db_admin"
+  policies = [ "arn:aws:iam::aws:policy/job-function/DatabaseAdministrator" ]
+}, {
+  name = "ro_user"
+  policies = [ "arn:aws:iam::aws:policy/AmazonConnectReadOnlyAccess", "arn:aws:iam::aws:policy/AIOpsReadOnlyAccess" ]
+}]
 }
---
+++ modules/iam_module/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+    source = "aws"
+    version = "5.94.1"
+    }
+  }
+}
+
+locals {
+  #Creates a map of {user = job}
+  user_jobs = {for x in var.users : x.name => x.job}
+  #Finds each unique job type and creates a list
+  job_types = toset([for x in var.users : x.job]) 
+  #Creates an object where each policy is associated with its job
+  permissions = merge([for prms in var.permissions : {for plc in prms.policies[*] : plc => prms.name}]...)
+}
+
+resource "aws_iam_user" "default" {
+  for_each = local.user_jobs
+  name = each.key
+}
+
+resource "aws_iam_group" "default" {
+  for_each = local.job_types
+  name = each.value
+  depends_on = [ aws_iam_user.default ]
+}
+
+resource "aws_iam_group_membership" "default" {
+  for_each = local.job_types
+  name = "${each.value}-membership"
+  group = each.value
+  users = [
+    for x in var.users : x.name if x.job == each.value
+  ]
+  depends_on = [ aws_iam_group.default ]
+}
+
+resource "aws_iam_account_password_policy" "default" {
+  minimum_password_length        = 9
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = true
+  allow_users_to_change_password = true
+}
+
+resource "aws_iam_group_policy_attachment" "default" {
+  for_each = local.permissions
+  group = each.value
+  policy_arn = each.key
+  depends_on = [ aws_iam_group.default ]
+}
